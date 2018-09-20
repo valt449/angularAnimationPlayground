bbd0cf2794ff880360a9a8f7726a6519850c85da
Nicolaj Iversen <NII@kmd.dk>
2018-09-20 21:36:27 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { trigger, state, style, transition, keyframes, animate,  } from "@angular/animations";
+
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<button [@myTrigger]='state' (click)='toogle()'> Mybutton</button>
+  <ul>
+    <li *ngFor="let item of items" [@myTrigger]='state'>{{item}}</li>
+  </ul>
+  `,
+  styles: ['button { margin: 60px;}'],
+  animations: [
+    trigger('myTrigger', [
+          state('small', style({
+            transform: 'scale(1)'            
+          })),
+          state('large', style({
+            transform: 'scale(2)'
+          })),
+          state('fadeIn', style({
+            opacity: 1
+          })),
+          //transition('* <=> *', animate('300ms ease-out')),
+          transition('void => *', [
+            style({opacity: '0', transform: 'translateY(30px)' }),
+            animate('300ms')
+          ])
+  ])
+]
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent  { 
+  name = 'Angular'; 
+  state: string = 'fadeIn';
+  items = ['item1', 'item2', 'item3'];
+
+
+  public toogle() {
+    //this.state = (this.state === 'small' ? 'large' : 'small');
+    this.items.push('Anopther item');
+  }
 }
